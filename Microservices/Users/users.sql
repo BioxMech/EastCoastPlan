ae93438cb4be0259723a64e53fc6fa491d631037
jaydenhojingrong <jinglong05@hotmail.com>
2021-04-04 22:24:35 +0800

    updated users again and again

--- Microservices/Users/users.sql
+++ Microservices/Users/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 04, 2021 at 10:55 AM
+-- Generation Time: Apr 04, 2021 at 02:21 PM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -20,9 +20,9 @@ SET time_zone = "+00:00";
 --
 -- Database: `users`
 --
-DROP DATABASE IF EXISTS `users`
-CREATE DATABASE IF NOT EXISTS `users`DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `facility`;
+DROP DATABASE IF EXISTS `users`;
+CREATE DATABASE IF NOT EXISTS `users` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `users`;
 -- --------------------------------------------------------
 
 --
