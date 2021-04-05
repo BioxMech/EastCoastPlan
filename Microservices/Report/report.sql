e93479c9e4a99c7812ee9ca7f43810d41625ea01
Jason <70564653+BioxMech@users.noreply.github.com>
2021-04-05 15:34:59 +0800

    Report SQL Created

---
+++ Microservices/Report/report.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 05, 2021 at 07:34 AM
+-- Server version: 8.0.18
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `report`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+DROP TABLE IF EXISTS `report`;
+CREATE TABLE IF NOT EXISTS `report` (
+  `report_id` varchar(8) NOT NULL,
+  `date` varchar(6) DEFAULT NULL,
+  `time` varchar(4) DEFAULT NULL,
+  `message` varchar(300) DEFAULT NULL,
+  `facility_id` varchar(8) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `report`
+--
+
+INSERT INTO `report` (`report_id`, `date`, `time`, `message`, `facility_id`) VALUES
+('01', '220197', '1000', 'I am the 1st report of the table. Please do me well.', '0'),
+('02', '270521', '1322', 'Why so many report?', '02'),
+('03', '230321', '0152', 'POSTMAN added report 03', '03');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
