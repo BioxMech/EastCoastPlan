21030a0279cc006502c324b4d889314d96b94fba
PetrasTYR <PetrasTYR@users.noreply.github.com>
2021-04-12 17:39:22 +0800

    Complex edit facilities JAYDEN

--- complex_edit_facility.py
+++ complex_edit_facility.py
@@ -2,13 +2,11 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os, sys
 from os import environ
-
 import requests
 from invokes import invoke_http
 import amqp_setup
-
-import json
 import pika
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -18,44 +16,43 @@ facilities_URL = environ.get("facilities_URL") or "http://localhost:5002/updateA
 
 @app.route("/edit_facility", methods=['POST'])
 def edit_facility():
-    # request = json.dumps(request)
     if request.is_json:
         try:
             edit = request.get_json()
-            # print("\nReceived facility in JSON:", data)
-            # print (data["message"])
-            #returns the whole facilty in JSON
             result = processEditFacility(edit, facilities_URL)
-            code = result["code"]
+            code = result['code']
             message = json.dumps(result)
-            
-            if (code == 200):
+            if result['code'] == 200:
+                # return jsonify({
+                #     "code": 201,
+                #     "data": result
+                # }), 201
                 print('\n\n-----Publishing the (notifications) message with routing_key=user.notifications-----')
                 
-                amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="user.notifications", body=message, properties=pika.BasicProperties(delivery_mode = 2)) 
+                try:
+                    amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="user.notifications", body=message, properties=pika.BasicProperties(delivery_mode = 2)) 
+                    return jsonify({
+                        "code": 200,
+                        "message": "Message: Uploaded to DB"
+                    }), 200
+
+                except Exception as e:
+                    return jsonify({
+                        "code": 500,
+                        "error": str(e)
+                    }),500
+            else:
                 return jsonify({
-                    "code": 200,
-                    "message": "Message: Uploaded to DB"
-                }), 200
-
-                
-
+                    "code": 501,
+                    "result": result
+                }), 501
         except Exception as e:
-            print(e)
             return jsonify({
-                "code": 500,
-                "error:": str(e)
-            })  # do nothing.
+                "code": 502,
+                "message": str(e)
+            }), 502
 
-    # if reached here, not a JSON request.
-    return jsonify({
-        "code": 400,
-        "message": "Invalid JSON input: ",
-        "error:": str(request.get_data()),
-        "type": str(type(request))
-    }), 400
 
-#update availability based on id
 def processEditFacility(edit, facilities_URL):
     facility_id = edit["facility_id"]
     messages = edit["messages"]
@@ -65,7 +62,11 @@ def processEditFacility(edit, facilities_URL):
     facilities_result['data']['messages'] = messages
     facilities_result['data']['receiver'] = "user"
     return(facilities_result)
-    
-# Execute this program if it is run as a main script (not by 'import')
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5100, debug=True)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5100, debug=True)
--- editFacilities.Dockerfile
+++ editFacilities.Dockerfile
@@ -2,6 +2,12 @@ FROM python:3-slim
 WORKDIR /usr/src/app
 COPY ./Microservices/Notifications/requirements.txt ./Microservices/Notifications/
 RUN pip install --no-cache-dir -r ./Microservices/Notifications/requirements.txt
-COPY ./complex_editFacilities.py ./invokes.py ./amqp_setup.py ./
-CMD [ "python", "complex_editFacilities.py" ]
+COPY ./complex_edit_facility.py ./invokes.py ./amqp_setup.py ./
+CMD [ "python", "complex_edit_facility.py" ]
 
+# FROM python:3-slim
+# WORKDIR /usr/src/app
+# COPY ./Microservices/Notifications/requirements.txt ./
+# RUN pip install --no-cache-dir -r requirements.txt
+# COPY ./complex_edit_facilities.py ./invokes.py ./amqp_setup.py ./
+# CMD [ "python", "complex_edit_facilities.py" ]
